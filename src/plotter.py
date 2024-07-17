7d3dd5ebfcf4a171bbd04c987ea9d048b39c4469
Alec <alecmkrueger@tamu.edu>
2024-07-17 14:11:29 -0500

    adjust so that buoy data works too

--- src/plotter.py
+++ src/plotter.py
@@ -3,35 +3,24 @@ import pandas as pd
 import datetime
 import xarray as xr
 import matplotlib.pyplot as plt
-from plotter_classes import SurfacePlot,Glider,DepthPlot,Bounds,Histogram
+# from plotter_classes import SurfacePlot,Glider,DepthPlot,Bounds,Histogram
+from plotter_classes import SurfacePlot,DepthPlot,Histogram
+from data_classes import Buoy, Glider,CTD
+from bounds import Bounds
 from utils.plotter_utils import interp_data,filter_var
 
-ds = xr.open_dataset('../test_data/2024_mission_44.nc')
+ds = xr.open_dataset('../test_data/tabs225m01_K.nc')
+ds = ds.sel(date=slice('2021',None))
+df = ds[['e','v']].to_dataframe().reset_index()
+df['e'] = filter_var(df['e'],min_value=-50,max_value=50)
+df['v'] = filter_var(df['v'],min_value=-50,max_value=50)
 
-df = interp_data(ds)
-
-df['salinity'] = filter_var(df['salinity'],30,40)
-
-
-df = df.where(df.time>pd.to_datetime('2024-01-01'))
-df. dropna()
-df = df[::100]
-
-
-temperature = df['temperature'].to_numpy()
-salinity = df['salinity'].to_numpy()
-latitude = df['latitude'].to_numpy()
-longitude = df['longitude'].to_numpy()
-depth = df['pressure'].to_numpy()
-dates = df['time'].to_numpy()
-
-
-glider = Glider(lat = latitude,
-                lon = longitude,
-                depth = depth,
-                time = dates,
-                temperature = temperature,
-                salinity = salinity)
+buoy = Buoy(lat = np.array([21]),
+            lon = np.array([-85]),
+            depth= df['depth'].to_numpy(),
+            time = df['date'].to_numpy(),
+            u_current=df['e'].to_numpy(),
+            v_current=df['v'].to_numpy())
 
 bounds = Bounds(lat_min=18,
                 lat_max=24,
@@ -40,23 +29,24 @@ bounds = Bounds(lat_min=18,
                 depth_bottom=1000,
                 depth_top=None)
 
-fig,axes = plt.subplots(nrows=8,figsize = (10,35))
-surfaces = SurfacePlot(instrument=glider,bounds=bounds)
-surfaces.map(fig=fig,ax=axes[0])
-surfaces.map(fig=fig,ax=axes[1],var='temperature',surface_values=True)
+# fig,axes = plt.subplots(nrows=8,figsize = (10,35))
+# surfaces = SurfacePlot(instrument=buoy,bounds=bounds)
+# surfaces.map(fig=fig,ax=axes[0])
+# surfaces.map(fig=fig,ax=axes[1],var='u_current',surface_values=False)
+# surfaces.map(fig=fig,ax=axes[2],var='v_current',surface_values=False)
 
-depth_plot = DepthPlot(instrument=glider,bounds=bounds)
+depth_plot = DepthPlot(instrument=buoy,bounds=bounds)
 
-depth_plot.time_series(fig=fig,ax=axes[2],var='temperature')
-depth_plot.var_var(fig=fig,ax=axes[3],x='salinity',y='temperature',color_var='salinity')
-depth_plot.var_var(fig=fig,ax=axes[4],x='temperature',y='depth',color_var='salinity')
-axes[4].invert_yaxis()
+depth_plot.time_series(var='u_current')
+plt.show()
+depth_plot.time_series(var='v_current')
+plt.show()
 
-hist = Histogram(instrument=glider,bounds=bounds)
-hist.plot(fig=fig,ax=axes[5],var='temperature')
-hist.plot(fig=fig,ax=axes[6],var='salinity')
-hist.plot2d(fig=fig,ax=axes[7],x='temperature',y='depth',range=[[0, 35], [0, 1000]],bins=100,norm='log')
+hist = Histogram(instrument=buoy,bounds=bounds)
+hist.plot(var='u_current')
+plt.show()
+hist.plot(var='v_current')
+plt.show()
+hist.plot2d(x='u_current',y='v_current',bins=100,range=[[-100, 100], [-100, 100]],norm='log')
 hist.ax.invert_yaxis()
-
-
 plt.show()
