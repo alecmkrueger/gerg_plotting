7ba014e6deaeee5ae6b31ae882a4b460ae1344bf
alecmkrueger <alecmkrueger@gmail.com>
2024-08-01 12:57:35 -0500

    add cmaps and units classes

--- src/gerg_plotting/classes_data.py
+++ src/gerg_plotting/classes_data.py
@@ -11,23 +11,43 @@ from gerg_plotting.classes_utils import get_center_of_mass,lat_min_smaller_than_
 
 @define
 class NonSpatialData:
-    def __getitem__(self, key:str):
-        return asdict(self)[key]
+    def __getitem__(self, key):
+        if key in self.__annotations__:
+            return getattr(self, key)
+        raise KeyError(f"Attribute '{key}' not found")
+    
+    def __setitem__(self, key, value):
+        if key in self.__annotations__:
+            setattr(self, key, value)
+        else:
+            raise KeyError(f"Attribute '{key}' not found")
     def __repr__(self):
         '''Pretty printing'''
         return pformat(asdict(self), indent=1,width=2,compact=True,depth=1)
+    
+@define
+class CMaps(NonSpatialData):
+    temperature:Colormap = field(default=cmocean.cm.thermal)
+    salinity:Colormap = field(default=cmocean.cm.haline)
+    density:Colormap = field(default=cmocean.cm.dense)
+    depth:Colormap = field(default=cmocean.tools.crop_by_percent(cmocean.cm.deep,7,'both'))
+    u_current:Colormap = field(default=cmocean.cm.delta)
+    v_current:Colormap = field(default=cmocean.cm.delta)
+
+@define
+class Units(NonSpatialData):
+    temperature:str = field(default='°C')
+    salinity:str = field(default='')
+    density:str = field(default="kg/m\u00B3")
+    depth:str = field(default='m')
+    u_current:str = field(default='cm/s')
+    v_current:str = field(default='cm/s')
+
 
 class Lab(NonSpatialData):
     def __init__(self,vars):
         for key,value in vars.items():
             setattr(self,key,value)
-    def __getitem__(self, key:str):
-        return getattr(self,key)
-    def __setitem__(self,key,value):
-        return setattr(self,key,value)
-    def __repr__(self):
-        '''Pretty printing'''
-        return pformat(self.__dict__, indent=1,width=2,compact=True,depth=1)
 
 @define
 class Bounds(NonSpatialData):
@@ -48,14 +68,24 @@ class SpatialData:
     lon:np.ndarray = field(default=None)
     depth:np.ndarray = field(default=None)
     time:np.ndarray = field(default=None)
-    # Cmaps
-    temperature_cmap:Colormap = field(default=cmocean.cm.thermal)
-    salinity_cmap:Colormap = field(default=cmocean.cm.haline)
-    u_current_cmap:Colormap = field(default=cmocean.cm.delta)
-    v_current_cmap:Colormap = field(default=cmocean.cm.delta)
+    cmaps:CMaps = field(factory=CMaps)
+    units:Units = field(factory=Units)
 
-    def __getitem__(self, key:str):
-        return asdict(self)[key]
+    def has_var(self,var):
+        if var in asdict(self).keys():
+            return True
+        else:
+            return False
+    def __getitem__(self, key):
+        if key in self.__annotations__:
+            return getattr(self, key)
+        raise KeyError(f"Attribute '{key}' not found")
+    
+    def __setitem__(self, key, value):
+        if key in self.__annotations__:
+            setattr(self, key, value)
+        else:
+            raise KeyError(f"Attribute '{key}' not found")
     def __repr__(self):
         '''Pretty printing'''
         return pformat(asdict(self), indent=1,width=2,compact=True,depth=1)
@@ -107,6 +137,7 @@ class Glider(SpatialData):
     # Vars
     temperature:np.ndarray = field(default=None)
     salinity:np.ndarray = field(default=None)
+    density:np.ndarray = field(default=None)
 
 @define
 class Buoy(SpatialData):
