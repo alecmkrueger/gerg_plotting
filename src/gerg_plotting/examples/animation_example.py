5ae146bec54b15252256c0f902efe5b4fb16b068
Alec Krueger <alecmkrueger@tamu.edu>
2024-10-30 17:10:23 -0400

    add comments

--- src/gerg_plotting/examples/animation_example.py
+++ src/gerg_plotting/examples/animation_example.py
@@ -1,4 +1,4 @@
-from gerg_plotting import Data,Histogram,Animator,Variable
+from gerg_plotting import Data,Variable,Histogram,Animator
 from gerg_plotting.utils import calculate_range
 import numpy as np
 
@@ -11,11 +11,13 @@ data.add_custom_variable(pH)
 temp_range=calculate_range(data['temperature'].data)
 
 def make_hists(sample,data=data):
-    data_sample = data[:10*sample+1]
-    hist = Histogram(data_sample)
-    hist.plot('temperature',color='g',bins=30)
+    '''Plot Histogram based on sample size'''
+    data_sample = data[:10*sample+1]  # Slice data
+    hist = Histogram(data_sample)  # Init histogram plotter
+    hist.plot('temperature',color='g',bins=30,range=(25,31))  # Plot 1-d histogram
+    hist.ax.set_ybound(upper=80)  # Set the ybounds maximum to 80 for a clearer plot
     return hist.fig
 
-Animator().animate(plotting_function=make_hists,iterable=np.arange(90),iteration_param='sample',gif_filename='hist.gif')
+Animator().animate(plotting_function=make_hists,iterable=range(90),iteration_param='sample',gif_filename='hist.gif')
 
 
