810798ea8f6f46b5455b824c9c25584162a90b45
Alec Krueger <alecmkrueger@tamu.edu>
2024-10-31 16:51:30 -0400

    add more methods

--- src/gerg_plotting/plotting_classes/Plotter3D.py
+++ src/gerg_plotting/plotting_classes/Plotter3D.py
@@ -17,6 +17,14 @@ class Plotter3D:
         fig = mlab.figure()
         raise NotImplementedError('Need to add method for initializing the mayavi figure')
     
+    def _has_var(self, key) -> bool:
+        '''Check if object has var'''
+        return key in asdict(self).keys()
+    
+    def _get_vars(self) -> list:
+        '''Get list of object variables/attributes'''
+        return list(asdict(self).keys())
+
     def __getitem__(self, key: str):
         '''
         Allow dictionary-style access to class attributes.
@@ -27,7 +35,16 @@ class Plotter3D:
         Returns:
             The value of the specified attribute.
         '''
-        return asdict(self)[key]
+        if self._has_var(key):
+            return getattr(self, key)
+        raise KeyError(f"Variable '{key}' not found. Must be one of {self._get_vars()}")  
+
+    def __setitem__(self, key, value):
+        """Allows setting standard and custom variables via indexing."""
+        if self._has_var(key):
+            setattr(self, key, value)
+        else:
+            raise KeyError(f"Variable '{key}' not found. Must be one of {self._get_vars()}")
 
     def __repr__(self):
         '''Return a pretty-printed string representation of the class attributes.'''
