f92d9baac4bea6c1d17efeb1fd42f9c959e09fcb
Alec Krueger <alecmkrueger@tamu.edu>
2024-11-01 15:20:11 -0400

    add example code

--- src/gerg_plotting/examples/three_d_example.py
+++ src/gerg_plotting/examples/three_d_example.py
@@ -1,4 +1,5 @@
-from gerg_plotting import Data,Scatter3D
+import mayavi.mlab
+from gerg_plotting import Data,ScatterPlot3D
 import pandas as pd
 
 # Let's read in the example data
@@ -9,4 +10,9 @@ df['pressure'] = df['pressure']/-1000
 data = Data(lat=df['latitude'],lon=df['longitude'],depth=df['pressure'],time=df['time'],
             salinity=df['salinity'],temperature=df['temperature'],density=df['density'])
 
-Scatter3D(data).plot()
+# Init the 3-d scatter plot
+three_d = ScatterPlot3D(data)
+three_d.plot('salinity',show=False)
+fig = three_d.fig
+three_d.plot('temperature',show=False,fig=fig)
+three_d.show()
