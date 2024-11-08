7381d78634db40c30ad50fa0692a68dd3daec022
Alec Krueger <alecmkrueger@tamu.edu>
2024-11-08 16:17:41 -0600

    create example for adding a custom Bathy object to the 3d plotting class

---
+++ src/gerg_plotting/examples/custom_bathy.py
@@ -0,0 +1,12 @@
+from gerg_plotting import ScatterPlot3D,Bounds,Bathy,data_from_csv
+
+# Let's read in the example data
+data = data_from_csv('example_data/sample_glider_data.csv')
+
+data_bounds = data.detect_bounds(bounds_padding=0.75)
+
+bathy = Bathy(bounds=data_bounds,resolution_level=5)
+
+# Init the 3-d scatter plot
+three_d = ScatterPlot3D(data,bathy=bathy)
+three_d.map(var='temperature',vertical_scalar=-1000)
