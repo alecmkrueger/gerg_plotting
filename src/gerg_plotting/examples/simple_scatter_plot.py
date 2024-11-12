2fdd586da11c11066d88ac58a416f33fe6520582
Alec Krueger <alecmkrueger@tamu.edu>
2024-11-11 18:47:17 -0600

    add a simple scatter plot example

---
+++ src/gerg_plotting/examples/simple_scatter_plot.py
@@ -0,0 +1,15 @@
+from gerg_plotting import Bounds,ScatterPlot,data_from_csv
+from gerg_plotting.utils import generate_random_point
+import numpy as np
+import pandas as pd
+import matplotlib.dates as mdates
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import cmocean
+
+# Define bounds
+bounds = Bounds(lat_min = 24,lat_max = 31,lon_min = -99,lon_max = -88,depth_top=-1,depth_bottom=1000)
+# Let's read in the example data
+data = data_from_csv('example_data/sample_glider_data.csv')
+
+scatter = ScatterPlot(data).scatter('time','depth')
