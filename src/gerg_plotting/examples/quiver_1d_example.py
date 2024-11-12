6113775d052f0866af512279c7d676ea3eab1401
Alec Krueger <alecmkrueger@tamu.edu>
2024-11-12 16:26:25 -0600

    update example

--- src/gerg_plotting/examples/quiver_1d_example.py
+++ src/gerg_plotting/examples/quiver_1d_example.py
@@ -1,6 +1,11 @@
-from gerg_plotting import ScatterPlot,data_from_csv
+from gerg_plotting import ScatterPlot,data_from_df
+import pandas as pd
 
 # Let's read in the example data
-data = data_from_csv('example_data/sample_radar_data.csv')
+df = pd.read_csv('example_data/sample_tabs_data.csv')
+# Group by the depth
+groups = [group for _,group in df.groupby('bin_depth')]
+# Init data from the first group
+data = data_from_df(groups[0])
 
-ScatterPlot(data).quiver1d(x='time',quiver_scale=1)
+ScatterPlot(data).quiver1d(x='time',quiver_scale=700)
