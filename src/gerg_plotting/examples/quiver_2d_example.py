ecfe019843a3e7002022c9f71858608572daa08f
Alec Krueger <alecmkrueger@tamu.edu>
2024-11-12 16:26:33 -0600

    update example

--- src/gerg_plotting/examples/quiver_2d_example.py
+++ src/gerg_plotting/examples/quiver_2d_example.py
@@ -1,10 +1,14 @@
-from gerg_plotting import Data,MapPlot,data_from_csv,data_from_df,Bathy
+from gerg_plotting import Data,MapPlot,data_from_csv,data_from_df,Bathy,ScatterPlot
 import matplotlib.pyplot as plt
 import pandas as pd
 import cmocean
 
-data = data_from_csv('example_data/sample_radar_data.csv')
+data = data_from_csv('example_data/sample_tabs_data.csv')
 
-MapPlot(data).quiver(quiver_density=200)
+data.speed.units = 'cm/s'
+
+scatter = ScatterPlot(data)
+scatter.quiver2d(x='time',y='depth',quiver_scale=800)
+scatter.ax.invert_yaxis()
 
 plt.show()
