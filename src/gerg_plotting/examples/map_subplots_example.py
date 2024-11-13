2bf8c61277d92197cf2f0c936f6ddd1b88257301
Alec Krueger <alecmkrueger@tamu.edu>
2024-11-13 17:15:27 -0600

    update example

--- src/gerg_plotting/examples/map_subplots_example.py
+++ src/gerg_plotting/examples/map_subplots_example.py
@@ -1,11 +1,6 @@
-from gerg_plotting import Data,MapPlot,Bounds,Variable,data_from_csv
-from gerg_plotting.plotting_classes.utils import generate_random_point
-import numpy as np
-import pandas as pd
-import matplotlib.dates as mdates
+from gerg_plotting import MapPlot,Bounds,data_from_csv
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
-import cmocean
 
 # Define bounds
 bounds = Bounds(lat_min = 24,lat_max = 31,lon_min = -99,lon_max = -88,depth_top=-1,depth_bottom=1000)
@@ -13,9 +8,6 @@ bounds = Bounds(lat_min = 24,lat_max = 31,lon_min = -99,lon_max = -88,depth_top=
 data = data_from_csv('example_data/sample_glider_data.csv')
 data.bounds = bounds
 
-
-# Init Data object
-# data = Data(lat=lats,lon=lons,salinity=salinity,temperature=temperature,depth=depth,time=time,bounds=bounds)
 # Init subplots
 fig,ax = plt.subplots(figsize=(10,15),nrows=4,subplot_kw={'projection': ccrs.PlateCarree()},layout='constrained')
 # Init MapPlot object
@@ -26,4 +18,4 @@ plotter.scatter(fig=fig,ax=ax[1],var='salinity',show_bathy=True,pointsize=30)
 plotter.scatter(fig=fig,ax=ax[2],var='depth',show_bathy=True,pointsize=30)
 plotter.scatter(fig=fig,ax=ax[3],var='time',show_bathy=True,pointsize=30)
 plt.show()
-# fig.savefig('example_plots/map_example.png',dpi=500,bbox_inches='tight')
+fig.savefig('example_plots/map_example.png',dpi=500,bbox_inches='tight')
