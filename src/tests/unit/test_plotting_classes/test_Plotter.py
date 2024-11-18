77833aedcb0aa5036d11a8c809bcd5d92a5b9f03
Alec Krueger <alecmkrueger@tamu.edu>
2024-11-18 15:41:22 -0600

    start adding tests

--- src/tests/unit/test_plotting_classes/test_Plotter.py
+++ src/tests/unit/test_plotting_classes/test_Plotter.py
@@ -1,2 +1,11 @@
-from gerg_plotting.plotting_classes import Plotter
+from gerg_plotting.plotting_classes.Plotter import Plotter
+from gerg_plotting.tools import data_from_csv
+
+import unittest
+
+class testPlotter(unittest.TestCase):
+
+    def setUp(self):
+        self.data = data_from_csv('C:/Users/alecmkrueger/Documents/GERG/gerg_plotting/src/gerg_plotting/examples/example_data/sample_glider_data.csv')
+        self.plotter = Plotter(data=self.data,bounds_padding=0.25)
 
