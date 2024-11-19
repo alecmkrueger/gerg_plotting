4cfbda1c3c0fd6a99b5f54fd7ce777c7ed7a7c3a
Alec Krueger <alecmkrueger@tamu.edu>
2024-11-19 12:46:11 -0600

    add many more tests

--- src/tests/unit/test_plotting_classes/test_Plotter.py
+++ src/tests/unit/test_plotting_classes/test_Plotter.py
@@ -1,36 +1,129 @@
-from gerg_plotting.plotting_classes.Plotter import Plotter
-from gerg_plotting.tools import data_from_csv
-
 import unittest
-import numpy as np
+import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 from matplotlib.axes import Axes
+from matplotlib.colorbar import Colorbar
+from matplotlib.colors import Colormap
+import numpy as np
+from attrs import define,field
+
+from gerg_plotting.plotting_classes.Plotter import Plotter
+
+@define
+class DummyVariable:
+    """Dummy variable class with minimal attributes for testing."""
+    cmap: Colormap = None
+
+    def get_label(self):
+        return "Dummy Variable"
+
+@define
+class DummyData:
+    """Dummy Data class to simulate Plotter dependencies."""
+    variables: dict = field(factory=dict)
+
+    def __getitem__(self, key):
+        return self.variables[key]
 
 class TestPlotter(unittest.TestCase):
     def setUp(self):
-        """
-        Set up the test environment for Plotter with basic data.
-        """
-        self.data = {
-            "x": np.linspace(0, 10, 100),
-            "y": np.sin(np.linspace(0, 10, 100)),
-        }
-        self.plotter = Plotter()
+        # Create a DummyData instance with a dummy variable
+        dummy_var = DummyVariable(cmap=plt.get_cmap('viridis'))
+        dummy_time_var = DummyVariable(cmap=plt.get_cmap('viridis'))
+        self.data = DummyData(variables={'test_var': dummy_var,'time':dummy_time_var})
+        self.plotter = Plotter(data=self.data)
+        # Fake the cbar attribute
+        self.plotter.cbar = None
 
-    def test_create_figure(self):
-        """
-        Test that create_figure initializes a matplotlib figure with correct attributes.
-        """
-        fig, ax = self.plotter.create_figure()
-        self.assertIsInstance(fig, Figure)
-        self.assertIsInstance(ax, Axes)
+    def tearDown(self):
+        plt.close()
 
-    def test_scatter_plot(self):
-        """
-        Test that a scatter plot is created and data is plotted correctly.
-        """
-        fig, ax = self.plotter.create_figure()
-        self.plotter.scatter(ax=ax, x=self.data["x"], y=self.data["y"])
-        scatter_points = ax.collections[0]  # Scatter plot data is stored in `collections`
-        self.assertEqual(len(scatter_points.get_offsets()), len(self.data["x"]))
+    def test_setitem(self):
+        with self.assertRaises(KeyError):
+            self.plotter['nonexistent']
+
+    def test_init_figure_2d(self):
+        """Test initializing a standard 2D figure."""
+        self.plotter.init_figure(figsize=(8, 6))
+        self.assertIsInstance(self.plotter.fig, Figure)
+        self.assertIsInstance(self.plotter.ax, Axes)
+        self.assertEqual(self.plotter.fig.get_size_inches().tolist(), [8, 6])
+
+    def test_init_figure_geography(self):
+        """Test initializing a figure with geographic projection."""
+        self.plotter.init_figure(geography=True)
+        self.assertIsNotNone(self.plotter.ax.projection)
+
+    def test_init_figure_3d(self):
+        """Test initializing a 3D figure."""
+        self.plotter.init_figure(three_d=True)
+        self.assertEqual(self.plotter.ax.name, '3d')
+
+    def test_init_figure_3d_and_geography(self):
+        """Test initializing a 3D with geography figure."""
+        with self.assertRaises(ValueError):
+            self.plotter.init_figure(three_d=True,geography=True)
+
+    def test_init_figure_3d_with_existing_axes(self):
+        """Test initializing a 3D figure."""
+        fig,ax = plt.subplots()
+        self.plotter.init_figure(fig=fig,ax=ax,three_d=True)
+        self.assertEqual(self.plotter.ax.name, '3d')
+
+    def test_get_cmap(self):
+        """Test retrieving a colormap for a variable."""
+        cmap = self.plotter.get_cmap('test_var')
+        self.assertIsInstance(cmap, Colormap)
+        self.assertEqual(cmap.name, 'viridis')
+
+    def test_get_cmap_missing_cmap(self):
+        """Test retrieving a colormap for a variable."""
+        test_data = self.data
+        test_data['test_var'].cmap = None
+        self.plotter.data = test_data
+        cmap = self.plotter.get_cmap('test_var')
+        self.assertIsInstance(cmap, Colormap)
+        self.assertEqual(cmap.name, 'viridis')
+
+    def test_add_colorbar(self):
+        """Test adding a colorbar."""
+        self.plotter.init_figure()
+        scatter = self.plotter.ax.scatter([1, 2, 3], [4, 5, 6], c=[0.1, 0.5, 0.9])
+        cbar = self.plotter.add_colorbar(mappable=scatter, var='test_var')
+        self.assertIsInstance(cbar, Colorbar)
+        self.assertEqual(cbar.ax.get_ylabel(), 'Dummy Variable')
+
+    def test_add_colorbar_with_time(self):
+        """Test adding a colorbar."""
+        self.plotter.init_figure()
+        scatter = self.plotter.ax.scatter([1, 2, 3], [4, 5, 6], c=[0.1, 0.5, 0.9])
+        cbar = self.plotter.add_colorbar(mappable=scatter, var='time')
+        self.assertIsInstance(cbar, Colorbar)
+        self.assertEqual(cbar.ax.get_ylabel(), 'Dummy Variable')
+
+    def test_format_axes(self):
+        """Test formatting the axes."""
+        self.plotter.init_figure()
+        self.plotter.format_axes('X Axis', 'Y Axis', invert_yaxis=True)
+        self.assertEqual(self.plotter.ax.get_xlabel(), 'X Axis')
+        self.assertEqual(self.plotter.ax.get_ylabel(), 'Y Axis')
+        self.assertTrue(self.plotter.ax.yaxis_inverted())
+
+    def test_dict_access(self):
+        """Test dictionary-style access."""
+        self.plotter['bounds_padding'] = 10
+        self.assertEqual(self.plotter['bounds_padding'], 10)
+        with self.assertRaises(KeyError):
+            _ = self.plotter['nonexistent']
+
+    def test_repr(self):
+        """Test the string representation of the class."""
+        repr_str = repr(self.plotter)
+        self.assertIn('bounds_padding', repr_str)
+
+    def test_get_vars(self):
+        """Test retrieving a list of variables."""
+        vars_list = self.plotter.get_vars()
+        self.assertIn('data', vars_list)
+        self.assertIn('fig', vars_list)
 
