387a10972651bd4f38726b6d683e5b2a763a5d46
Alec Krueger <alecmkrueger@tamu.edu>
2024-11-20 15:40:21 -0600

    add test

--- src/tests/unit/test_examples/test_custom_3d_bathy_example.py
+++ src/tests/unit/test_examples/test_custom_3d_bathy_example.py
@@ -1,2 +1,15 @@
 from gerg_plotting.examples.custom_3d_bathy_example import custom_3d_bathy_example
 
+import unittest
+import os
+from pathlib import Path
+
+class test_custom_3d_bathy_example(unittest.TestCase):
+
+    def test(self):
+        image_path = Path('example_plots/custom_3d_bathy_example.png')
+        if image_path.exists():
+            os.remove(image_path)
+        custom_3d_bathy_example()
+        self.assertTrue(image_path.exists())
+
