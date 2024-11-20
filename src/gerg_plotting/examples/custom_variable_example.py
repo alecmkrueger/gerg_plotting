f361dce695699fa55c6f62b271fa27a96fe7563a
Alec Krueger <alecmkrueger@tamu.edu>
2024-11-20 14:39:59 -0600

    clean up

--- src/gerg_plotting/examples/custom_variable_example.py
+++ src/gerg_plotting/examples/custom_variable_example.py
@@ -1,11 +1,12 @@
-from gerg_plotting import ScatterPlot,Histogram,Variable,data_from_df,ScatterPlot3D
+from gerg_plotting import Histogram,Variable,data_from_df
 from gerg_plotting.modules.plotting import get_turner_cmap
 import pandas as pd
+from pathlib import Path
 
 
 def custom_variable_example():
     # Let's read in the example data
-    df = pd.read_csv('example_data/sample_glider_data_test.csv')
+    df = pd.read_csv('example_data/sample_glider_data.csv')
 
     # Let's initilize the data object
     data = data_from_df(df)
@@ -17,11 +18,9 @@ def custom_variable_example():
     # Add the Turner_Rsubrho Variable object to the Data object
     data.add_custom_variable(Turner_Rsubrho)
     # Test by plotting a histogram
-    Histogram(data).plot(var='Turner_Rsubrho')
-    # Plot hovmoller 
-    ScatterPlot(data).hovmoller(var='Turner_Rsubrho')
-    # Plot 3d map
-    ScatterPlot3D(data).map(var='Turner_Rsubrho',vertical_scalar=-1000)
+    hist = Histogram(data)
+    hist.plot(var='Turner_Rsubrho')
+    hist.save('example_plots/custom_variable_example.png')
 
 
 if __name__ == "__main__":
