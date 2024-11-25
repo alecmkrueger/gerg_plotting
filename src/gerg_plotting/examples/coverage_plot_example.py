3b209db82958126dd0895695f2e659623ba15d4a
Alec Krueger <alecmkrueger@tamu.edu>
2024-11-25 12:46:00 -0600

    update plot

--- src/gerg_plotting/examples/coverage_plot_example.py
+++ src/gerg_plotting/examples/coverage_plot_example.py
@@ -4,43 +4,36 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-# palette = ['#275C62', '#298880', '#5AA786','#8AB17D','#E9C46A','#F4A261','#E76F51']
-# palette = ['#298880', '#8AB17D', '#BABB74','#E9C46A','#F4A261','#EE8959','#E76F51']
-
-# cmap = matplotlib.colors.ListedColormap(palette)
-# n_colors = len(palette)
-
 def coverage_plot_example():
+    # Define the cmap we will use
     cmap = 'tab20'
-    n_colors = 20
-
-    x_labels = ['Seconds','Minutes','Hours','Days','Weeks','Month','Years','Decades']
-    y_labels = ['Surface','10-100\nmeters','100-500\nmeters','Below 500\nmeters','Benthos']
-
-
-    fig,ax = plt.subplots(figsize=(11,7))
-
+    # Define the x and y labels
+    x_labels = ['Seconds','Minutes','Hours','Days','Weeks','Months','Years','Decades']
+    y_labels = ['Surface','10-100\nMeters','100-500\nMeters','Below 500\nMeters','Benthos']
+    # Init the coverage plotter
     plotter = CoveragePlot(x_labels=x_labels,y_labels=y_labels,
-                        colormap=cmap,n_colors=n_colors)
-    # Init plot with the x and y labels and the axes bounds limit
-    plotter.set_up_plot(fig=fig,ax=ax,padding=0.25)
-    # Add grid
-    plotter.add_hlines(np.arange(-0.5,5.5,1),linewidth=1.25,ls='--',color='gray')
-    plotter.add_vlines(np.arange(0,9,1),linewidth=1.25,ls='--',color='gray')
+                        colormap=cmap,figsize=(12,6),coverage_fontsize=10.5,
+                        grid_linestyle='--',grid_linewidth=0.9)
+    # Init plot with the x and y labels and the axes bounds limit, must do before adding coverages
+    plotter.set_up_plot(horizontal_padding=0.25,vertical_padding=0.75)
     # All Depths
     plotter.add_coverage(x_range=[7,8],y_range=[-0.45,4.2],label='Climate\nScience')
-    plotter.add_coverage(x_range=[5,6],y_range=[-0.45,4.2],label='Fisheries')
+    plotter.add_coverage(x_range=[5,7],y_range=[-0.45,4.2],label='Fisheries')
     # Surface
-    plotter.add_coverage(x_range=[3,7],y_range=[-0.15,-0.15],label='Oil and Gas')
-    plotter.add_coverage(x_range=[2,3],y_range=[-0.45,-0.45],label='SAR')
-    plotter.add_coverage(x_range=[3,7],y_range=[0.15,0.15],label='Wind and Algal Blooms')
+    plotter.add_coverage(x_range=[3,8],y_range=[-0.15,-0.15],label='Oil and Gas')
+    plotter.add_coverage(x_range=[2,4],y_range=[-0.45,-0.45],label='Search and Rescue')
+    plotter.add_coverage(x_range=[3,8],y_range=[0.15,0.15],label='Wind and Algal Blooms')
     # 10-100m
-    plotter.add_coverage(x_range=[2,4],y_range=[0.85,0.85],label='Hurricane Forcasting')
-    plotter.add_coverage(x_range=[3,6],y_range=[1.15,1.15],label='Hypoxia')
+    plotter.add_coverage(x_range=[2,5],y_range=[0.85,0.85],label='Hurricane Forcasting')
+    plotter.add_coverage(x_range=[3,7],y_range=[1.15,1.15],label='Hypoxia')
     # Below 500m
-    plotter.add_coverage(x_range=[3,7],y_range=[3,3],label='Oil and Gas',fc=plotter.colormap(2))
+    plotter.add_coverage(x_range=[3,8],y_range=[2.85,2.85],label='Oil and Gas',fc=plotter.colormap(2))
 
-    plotter.save('example_plots/coverage_plot_example.png')
+    plotter.ax.tick_params(axis='x', labeltop=True, labelbottom=False)
+
+    plotter.fig.tight_layout()
+
+    plotter.fig.savefig('example_plots/coverage_plot_example.png',dpi=600)
 
 
 if __name__ == "__main__":
