3f6032fd4080855f03e28bfa6decd0f8629d3746
Alec Krueger <alecmkrueger@tamu.edu>
2024-11-26 16:22:33 -0600

    create example for a complex coverage plot

--- src/gerg_plotting/examples/coverage_plot_example.py
+++ src/gerg_plotting/examples/coverage_plot_example.py
@@ -1,105 +1,55 @@
 from gerg_plotting.plotting_classes.CoveragePlot import CoveragePlot
+from gerg_plotting.tools import custom_legend_handles
 
 import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import cmocean
-from matplotlib.ticker import FixedLocator
 
 
 def coverage_plot_example():
-    df = pd.read_csv('example_data/stakeholder.csv')
-    df = df[5:].reset_index(drop=True)
-    df = df[df['index']!='TIME'].reset_index(drop=True)
-    df = df.T
-    df.columns = df.loc['index']
-    df = df.drop('index')
+    cmap = plt.get_cmap('tab20')
+    # domains = create_combinations_with_underscore(['Basin', 'Regional', 'Local'])
+    # domains.extend(['All'])
+    # print(domains)
+    domains = ['Regional_Local', 'All', 'Local', 'Basin_Regional', 'Basin_Local', 'Basin', 'Regional']
+    colors_light = [cmap(15),cmap(5),cmap(3),cmap(1),'yellow','pink','gold']
+    colors_dark = [cmap(14),cmap(4),cmap(2),cmap(0),'yellow','pink','gold']
 
-    def point_sum(df:pd.DataFrame,column1:str,column2:str):
-        xx = df[column1]
-        yy = df[column2]
-        total = 0
-        for x,y in zip(xx,yy):
-            if x ==1 and y==1:
-                total +=1
-        return total
+    colors = colors_light
+    domain_colors = {key:value for key,value in zip(domains,colors)}
+    # Define the x and y labels
+    x_labels = ['Seconds','Minutes','Hours','Days','Weeks','Months','Years','Decades']
+    y_labels = ['Surface','10-100\nMeters','100-500\nMeters','Below 500\nMeters','Benthic']
+    # Init the coverage plotter
+    plotter = CoveragePlot(figsize=(12,6),x_labels=x_labels,y_labels=y_labels,
+                           grid_color='k',grid_linestyle='--',grid_linewidth=1,
+                           arrow_facecolor='coverage_facecolor',arrow_linewidth=1,arrow_width=0.06,arrow_head_width=0.12,
+                           coverage_fontsize=10.5,coverage_linewidth=1.25,coverage_alpha=1,coverage_min_rectangle_height=0.25)
+    # All Depths
+    plotter.add_coverage(x_range=['Hours','Decades'],y_range=['Surface','Benthic'],label='Agency',label_position=(4,3.3),fc=domain_colors['All'])
+    plotter.add_coverage(x_range=['Seconds','Decades'],y_range=['Surface','Benthic'],label='Academic',label_position=(3.5,2),fc=domain_colors['All'])
+    plotter.add_coverage(x_range=['Days','Months'],y_range=['Surface','Benthic'],label='Consultants',label_position=(4.5,1.7),fc=domain_colors['Regional_Local'])
+    plotter.add_coverage(x_range=['Days','Years'],y_range=['Surface','Benthic'],label='Regulatory',label_position=(4.5,2.3),fc=domain_colors['Regional_Local'])
+    plotter.add_coverage(x_range=['Days','Decades'],y_range=['Surface','Benthic'],label='Oil and Gas',label_position=(4.5,3),fc=domain_colors['All'])
+    plotter.add_coverage(x_range=['Months','Years'],y_range=['Surface','Benthic'],label='Fisheries',label_position=(6,2.75),fc=domain_colors['Regional_Local'])
+    plotter.add_coverage(x_range='Decades',y_range=['Surface','Benthic'],label='Climate\nScience',label_position=(7.5,1.7),fc=domain_colors['Basin_Regional'])
+    plotter.add_coverage(x_range=['Hours','Weeks'],y_range=['Surface','Benthic'],label='Disaster',label_position=(4,2.75),fc=domain_colors['All'])
+    # Surface
+    plotter.add_coverage(x_range=['Hours','Days'],y_range=[-0.5,-0.5],label='Search and Rescue',fc=domain_colors['Local'])
+    plotter.add_coverage(x_range=['Days','Decades'],y_range=[0.25,0.25],label='Wind and Algal Blooms',fc=domain_colors['Local'])
+    plotter.add_coverage(x_range=['Weeks','Months'],y_range=[-0.5,-0.5],label='Shipping',label_position=(4.6,-0.375),fc=domain_colors['Basin'])
+    plotter.add_coverage(x_range=['Days','Years'],y_range=[-0.15,-0.15],label='Recreational',label_position=(4.5,-0.025),fc=domain_colors['Basin'])
+    # 10-100m
+    plotter.add_coverage(x_range=['Months','Years'],y_range=['Surface','100-500 Meters'],label='CCUS',label_position=(6,0.775),fc=domain_colors['Local'])
+    plotter.add_coverage(x_range=['Hours','Weeks'],y_range=[0.65,0.65],label='Hurricane Forcasting',fc=domain_colors['All'])
+    plotter.add_coverage(x_range=['Days','Years'],y_range=[1,1],label='Hypoxia',fc=domain_colors['Regional_Local'])
+    plotter.plot()
 
+    handles = custom_legend_handles(domain_colors.keys(),domain_colors.values())
+    plotter.fig.legend(handles=handles,bbox_to_anchor=(0.25, 0.39),framealpha=1,title='Domains')
 
-    depths = ['Surface','Midwater','Benthic']
-    times = ['Seconds', 'Minutes', 'Hours', 'Days','Weeks', 'Months', 'Years', 'Decades']
+    plotter.fig.tight_layout()
 
-    df_summary = pd.DataFrame(np.zeros((len(depths),len(times))))
-
-    for depth_idx,depth in enumerate(depths):
-        for time_idx,time in enumerate(times):
-            df_summary.at[depth_idx,time_idx] = point_sum(df,depth,time)
-
-    # Prepare data for 3D plotting
-    x, y = np.meshgrid(df_summary.columns, df_summary.index)
-    x = x.ravel()  # Flatten the mesh grid for plotting
-    y = y.ravel()
-    z = np.zeros_like(x)  # Base of the bars
-    dx = dy = 0.8  # Width and depth of the bars
-    dz = df_summary.values.ravel()  # Heights (flattened DataFrame values)
-    colors = plt.get_cmap("viridis")(dz)
-
-    # Plot the 3D histogram
-    fig = plt.figure(figsize=(10, 10))
-    ax = fig.add_subplot(111, projection='3d')
-
-    cmap=cmocean.cm.thermal(plt.Normalize(0,max(dz))(dz))
-    ax.bar3d(x, y, z, dx, dy, dz, shade=True, color=cmap)
-    ax.set_zlabel('Count')
-    ax.set_title('3D Histogram')
-
-    def add_labels(axis,labels):
-        if axis.lower() == 'x':
-            major_locator = ax.xaxis.set_major_locator
-            label_setter = ax.set_xticklabels
-            tick_positions = np.arange(0.5,len(labels)+0.5)  # Tick positions
-            
-        elif axis.lower() == 'y':
-            major_locator = ax.yaxis.set_major_locator
-            label_setter = ax.set_yticklabels  
-            tick_positions = np.arange(0,len(labels))  # Tick positions     
-
-        major_locator(FixedLocator(tick_positions))
-        label_setter(labels)
-
-    add_labels('y',depths)
-    add_labels('x',times)
-    ax.view_init(30, 70)
-    plt.show()
-
-
-
-# surface_seconds = 
-# def coverage_plot_example():
-#     cmap = 'tab20'
-#     # Define the x and y labels
-#     x_labels = ['Seconds','Minutes','Hours','Days','Weeks','Months','Years','Decades']
-#     y_labels = ['Surface','10-100\nMeters','100-500\nMeters','Below 500\nMeters','Benthos']
-#     # Init the coverage plotter
-#     plotter = CoveragePlot(x_labels=x_labels,y_labels=y_labels,colormap=cmap,
-#                            figsize=(12,6),coverage_fontsize=10.5,coverage_linewidth=3,
-#                            grid_linestyle='--',grid_linewidth=0.9,coverage_alpha=0.5,coverage_min_rectangle_height=0.25)
-#     # All Depths
-#     plotter.add_coverage(x_range=['Seconds','Decades'],y_range=['Surface','Benthos'],label='Academic')
-#     plotter.add_coverage(x_range=['Days','Months'],y_range=['Surface','Benthos'],label='Consultants')
-#     plotter.add_coverage(x_range=['Days','Years'],y_range=[-0.25,'Benthos'],label='Regulatory')
-#     plotter.add_coverage(x_range=['Days','Decades'],y_range=['Surface','Benthos'],label='Oil and Gas')
-#     plotter.add_coverage(x_range=['Months','Years'],y_range=['Surface','Benthos'],label='Fisheries')
-#     plotter.add_coverage(x_range='Decades',y_range=['Surface','Benthos'],label='Climate\nScience')
-
-#     # Surface
-#     plotter.add_coverage(x_range=['Hours','Days'],y_range=[-0.5,-0.5],label='Search and Rescue')
-#     plotter.add_coverage(x_range=['Days','Decades'],y_range=[-0.5,-0.5],label='Wind and Algal Blooms')
-#     # 10-100m
-#     plotter.add_coverage(x_range=['Hours','Weeks'],y_range=[0.5,0.5],label='Hurricane Forcasting')
-#     plotter.add_coverage(x_range=['Days','Years'],y_range=[0.5,0.5],label='Hypoxia')
-
-#     plotter.plot()
-#     # plotter.fig.savefig('example_plots/coverage_plot_example.png',dpi=600)
+    plotter.show()
+    plotter.fig.savefig('example_plots/coverage_plot_example.png',dpi=600)
 
 
 if __name__ == "__main__":
