cd420a255a456fe7176870b08f979e2dd4d664fb
Alec Krueger <alecmkrueger@tamu.edu>
2024-11-26 16:37:03 -0600

    update example

--- src/gerg_plotting/examples/tricontourf_example.py
+++ src/gerg_plotting/examples/tricontourf_example.py
@@ -1,8 +1,16 @@
 from gerg_plotting.plotting_classes.ScatterPlot import ScatterPlot
 from gerg_plotting.tools import data_from_csv
 
-data = data_from_csv('example_data/glider_data.csv')
 
-plotter = ScatterPlot(data=data)
+def tricontourf_example():
+    data = data_from_csv('example_data/sample_glider_data.csv')
 
-# plotter.
+    plotter = ScatterPlot(data=data)
+
+    plotter.tricontourf(x='time',y='depth',z='temperature')
+    plotter.ax.invert_yaxis()
+
+    plotter.save('example_plots/tricontourf_example.png')
+
+if __name__ == "__main__":
+    tricontourf_example()
