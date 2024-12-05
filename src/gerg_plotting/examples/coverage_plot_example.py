00da829f73c1dfee68231db8386a753b4a572377
Alec Krueger <Alecmkrueger@tamu.edu>
2024-12-05 12:39:08 -0600

    implement hatching

--- src/gerg_plotting/examples/coverage_plot_example.py
+++ src/gerg_plotting/examples/coverage_plot_example.py
@@ -23,7 +23,7 @@ def coverage_plot_example():
                            grid_color='k',grid_linestyle='--',grid_linewidth=0.75,
                            arrow_facecolor='coverage_facecolor',arrow_linewidth=0.75,arrow_width=0.05,arrow_head_width=0.12,
                            coverage_fontsize=12,coverage_linewidth=0.75,coverage_alpha=1,coverage_min_rectangle_height=0.25,
-                           coverage_label_background_pad=1)
+                           coverage_label_background_pad=1,coverage_hatch_linewidth=1.5)
     # All Depths
     plotter.add_coverage(['Hours','Decades'],['Surface','Benthic'],label='Agency',label_position=(4,3.3),fc=domain_colors['All'])
     plotter.add_coverage(['Seconds','Decades'],['Surface','Benthic'],label='Academic',label_position=(3.5,2),fc=domain_colors['All'])
@@ -41,17 +41,21 @@ def coverage_plot_example():
     plotter.add_coverage(['Days','Years'],[1,1],label='Hypoxia',fc=domain_colors['Regional_Local'])
 
     plotter.add_coverage('Decades',['Surface','Benthic'],label='Climate\nScience',label_position=(7.5,1.7),fc=domain_colors['Basin_Regional'])
+    # plotter.patches[-1].rect.set_facecolor('none')
+    # plotter.patches[-1].text.set_color('w')
+    # print(plotter.patches[-1].rect.get_facecolor())
     plotter.add_coverage(['Weeks','Months'],[-0.5,-0.5],label='Shipping',fc=domain_colors['Basin'])
     plotter.add_coverage(['Days','Years'],[-0.15,-0.15],label='Recreational',label_position=(4.5,-0.025),fc=domain_colors['Basin'])
 
-    plotter.plot(with_hatches=False)
+
+    plotter.plot(with_hatches=True)
 
     handles = custom_legend_handles(domain_colors.keys(),domain_colors.values())
     plotter.fig.legend(handles=handles,bbox_to_anchor=(0.25, 0.39),framealpha=1,title='Domains')
 
     plotter.fig.tight_layout()
 
-    plotter.fig.savefig('example_plots/coverage_plot_example.png',dpi=600)
+    plotter.fig.savefig('example_plots/coverage_plot_example_with_hatches1.png',dpi=600)
 
 
 if __name__ == "__main__":
