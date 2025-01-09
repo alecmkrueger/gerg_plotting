0b6594202772cffcb1123c7cd516f04aac126b07
Alec Krueger <Alecmkrueger@tamu.edu>
2025-01-09 15:25:30 -0600

    add readthedocs files

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,15 +13,26 @@ author = 'Alec Krueger'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.todo',
+    'autoapi.extension',
+    'sphinx.ext.inheritance_diagram'
+]
+
+autoapi_dirs = ['../../src/gerg_plotting']
+
+autoapi_member_order = ['groupwise']
 
 templates_path = ['_templates']
 exclude_patterns = []
-
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+# Napoleon settings
+napoleon_numpy_docstring = True
