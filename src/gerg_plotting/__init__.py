b85a435937e9b7cdd83b07fcafac8bddba056d40
Alec Krueger <Alecmkrueger@tamu.edu>
2025-01-13 12:30:36 -0600

    remove ScatterPlot3d from the init imports for speed improvements

--- src/gerg_plotting/__init__.py
+++ src/gerg_plotting/__init__.py
@@ -10,7 +10,7 @@ from .data_classes.Bathy import Bathy
 from .data_classes.Data import Data
 from .plotting_classes.MapPlot import MapPlot
 from .plotting_classes.ScatterPlot import ScatterPlot
-from .plotting_classes.ScatterPlot3D import ScatterPlot3D
+# from .plotting_classes.ScatterPlot3D import ScatterPlot3D
 from .plotting_classes.CoveragePlot import CoveragePlot
-from .tools.tools import data_from_df,data_from_csv,interp_glider_lat_lon
+from .tools.tools import data_from_df,data_from_csv,interp_glider_lat_lon,data_from_netcdf
 import cmocean
