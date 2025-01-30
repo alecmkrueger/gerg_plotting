299612eeba7128d2f062adc382c17b56a39b0269
Alec Krueger <Alecmkrueger@tamu.edu>
2025-01-30 16:37:36 -0600

    add _scatter_plot_defaults property.

--- src/gerg_plotting/plotting_classes/scatter_plot_3d.py
+++ src/gerg_plotting/plotting_classes/scatter_plot_3d.py
@@ -13,9 +13,27 @@ from gerg_plotting.data_classes.bathy import Bathy
 
 @define
 class ScatterPlot3D(Plotter3D):
-
-    def _add_bathy(self) -> None:
-        raise NotImplementedError
+    
+    _scatter_plot_defaults: dict = field(default=None)
+    
+    @property
+    def scatter_scalar_bar_defaults(self) -> dict:
+        """
+        Returns the default settings for the scatter plot.
+        """
+        if self._scatter_plot_defaults is None:
+            self._scatter_plot_defaults = dict(render_points_as_spheres=True, 
+                                               point_size=10, vertical=True, 
+                                               height=0.7, width = 0.08,
+                                               position_y=0.15, position_x=0.03, fmt="%.1f")
+        return self._scatter_plot_defaults
+    
+    @scatter_scalar_bar_defaults.setter
+    def scatter_plot_defaults(self, value: dict) -> None:
+        """
+        Sets the default settings for the scatter plot.
+        """
+        self._scatter_plot_defaults = value
     
     def make_points_3d(self,x:str,y:str,z:str) -> np.ndarray:
         """A helper to make a 3D NumPy array of points (n_points by 3)"""
@@ -37,12 +55,26 @@ class ScatterPlot3D(Plotter3D):
         else:
             color_label = None
             cmap = None
+            
+        
         # Add the mesh to the plotter
-        self.plotter.add_mesh(scatter_points, scalars=color_label, show_scalar_bar=False, cmap = cmap,render_points_as_spheres=True, point_size=10)
+        self.plotter.add_mesh(scatter_points, 
+                              scalars=color_label, 
+                              cmap=cmap, 
+                              show_scalar_bar=False,  
+                              render_points_as_spheres=self.scatter_scalar_bar_defaults['render_points_as_spheres'], 
+                              point_size=self.scatter_scalar_bar_defaults['point_size'])
+        
         if color_label is not None:
-            self.add_colorbar(title=color_label, height=0.5, vertical=True, position_x=0.025, position_y=0.04,fmt="%.1f")
+            self.add_colorbar(title=color_label,
+                              vertical=self.scatter_scalar_bar_defaults['vertical'], 
+                              height=self.scatter_scalar_bar_defaults['height'], 
+                              width=self.scatter_scalar_bar_defaults['width'],
+                              position_y=self.scatter_scalar_bar_defaults['position_y'], 
+                              position_x=self.scatter_scalar_bar_defaults['position_x'], 
+                              fmt=self.scatter_scalar_bar_defaults['fmt'])
 
-    def add_bathy(self):
+    def _add_bathy(self):
         # Get bathymetry data
         seafloor_data_path = Path(__file__).parent.parent.joinpath('seafloor_data/gom_srtm30_plus.txt')
         df = pd.read_csv(seafloor_data_path,sep='\t')
@@ -83,7 +115,7 @@ class ScatterPlot3D(Plotter3D):
                               clim=(0,filtered_df.z.max()),flip_scalars=False, lighting=True,annotations=annotations)
         
         
-        self.add_colorbar(title='Depth (m)', width=0.5, vertical=False, below_label='', fmt="%.1f")
+        self.add_colorbar(title='Depth (m)', height=0.7,position_y=0.15, position_x = 0.90, vertical=True, below_label='', fmt="%0.1f")
 
 
     def map(self, var: str | None = None) -> None:
@@ -92,5 +124,5 @@ class ScatterPlot3D(Plotter3D):
         y = 'lat'
         z = 'depth'
         self.scatter(x, y, z, var)
-        self.add_bathy()
+        self._add_bathy()
         
