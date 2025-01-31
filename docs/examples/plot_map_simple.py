e8be15d10d4dede39a19419887b8c318dd503bd2
Alec Krueger <Alecmkrueger@tamu.edu>
2025-01-31 14:58:08 -0600

    update example

--- docs/examples/plot_map_simple.py
+++ docs/examples/plot_map_simple.py
@@ -7,11 +7,7 @@ Example of a simple map plot.
 """
 from gerg_plotting import data_from_netcdf,MapPlot
 
-data = data_from_netcdf('example_data/sample_glider_data.nc',interp_glider=True)
-
-data.detect_bounds(bounds_padding=1.5)
-
-data.time.cmap = 'viridis'
+data = data_from_netcdf('example_data/sample_glider_data.nc',interp_glider=True,bounds_padding=1.5)
 
 plotter = MapPlot(data)
 plotter.scatter('time',show_bathy=True,show_coastlines=False)
