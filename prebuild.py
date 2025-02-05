47ff1e19861fe4207f1b281555b812c0384561c0
Alec Krueger <Alecmkrueger@tamu.edu>
2025-02-05 11:29:57 -0600

    add todo

--- prebuild.py
+++ prebuild.py
@@ -1,5 +1,7 @@
 import subprocess
 
+# TODO: Add testing
+
 # Scripts to make requirements files
 make_requirements = "poetry export --without-hashes -f requirements.txt --output requirements.txt"
 make_requirements_docs = "poetry export --without-hashes --with docs -f requirements.txt --output docs/requirements.txt"
