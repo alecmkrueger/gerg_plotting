980a743d27fd986e421bc3cfe7de268746a6c4a2
Alec Krueger <Alecmkrueger@tamu.edu>
2025-02-05 10:31:41 -0600

    update package version

--- docs/examples/how_to_import.py
+++ docs/examples/how_to_import.py
@@ -5,7 +5,6 @@ How to import gerg_plotting
 This example shows some of the different ways to import the gerg_plotting package.
 
 """
-#%%
 # Method 1: Import just what you need
 # ------------------------------------
 
@@ -17,7 +16,6 @@ from gerg_plotting import Data, Histogram
 data = Data(temperature=np.random.normal(28,size=1000))
 hist = Histogram(data)  # Assign the histogram plotter to a variable
 
-# %%
 # Method 2: Import the whole package
 # ------------------------------------
 import gerg_plotting as gp
